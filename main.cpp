ce5473b031c5645aa960fd6ebf414db0cf3c00fc
Hossein Khalilnasl <68067650+khalilnasl@users.noreply.github.com>
2022-08-23 00:31:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,19 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include <iostream>
 #include <fstream>
-#include <sys/time.h>
 #include <ctime>
-#include <algorithm>    // std::shuffle
-#include <array>        // std::array
-#include <random>       // std::default_random_engine
-#include <chrono>       // std::chrono::system_clock
-#include <functional>
+#include <algorithm>
+#include <array>        
+#include <random>
 #include <string>
-#include <unordered_map>   // hashFunction.cpp
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
 
-using std::cout; using std::endl;
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
-using std::chrono::system_clock;
 using namespace std;
-using std::vector;
+
+
+
 
 
 
@@ -34,12 +22,8 @@ void GenMac(string mac[], int size);
 
 
 
-
-
 int main() {
 
-    
-    
     
     int capacity = 8;
     string A[capacity];
@@ -50,11 +34,6 @@ int main() {
     
     //int mac1[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
     //int mac2[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
-    //int mac3[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
-   // int mac4[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
-   // int mac5[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
-   
-   
    
        
     //int mac1[6] = {0x00, 0x0d, 0x3f, 0xcd, 0x02, 0x5f};
@@ -76,35 +55,19 @@ int main() {
    // cout << getArrayLength << endl;
     
     
-
-    
     return 0;
-   
-    
     }
 
 
 
 
 void GenMac(string mac[], int size){
-
     for(int j=0; j < size; j++){
         int i, tp;
        // srand(time(NULL) + getpid());
         for (i = 0; i < 6; i++){
             tp = rand() % 256;
-            
             printf("%s%X%s", tp < 16 ? "0" : "", tp, i < 5 ? ":" : "\n");
-           
-            
         }
-        
-        
     };
-    
-    
 }
-
-
-
-
